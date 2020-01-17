bfb9ba55c7555b641ea57cfac960ffe67abc5549
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-17 03:08:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,8 @@ def introduction(name)
 phrase = "Hi, my name is #{name}."
   puts phrase
 end
+
+def introduction(name, language)
+phrase = "Hi, my name is #{name} an I am learning to prgram in #{language}."
+  puts phrase
+end
